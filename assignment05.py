b0b4d5d95b219ffbb27ecb0587af33719a9362cd
Mark Peters <mapeters101@earthlink.net>
2025-01-04 22:44:34 -0500

    Initial code for assignment05.

---
+++ assignment05.py
@@ -0,0 +1,18 @@
+print("Welcome to your Letter Grade Calculator\n")
+#ask the user to enter their cooresponding grade based on the variable name. Remember to wrap the input around a float() so that we can use decimals. Ex.) float(input("What's your grade in ...: "))
+
+math =
+science =
+english =
+history =
+worldLanguage =
+physicalEducation =
+
+#store the average of all of the grades into the gpa variable (average is the sum of all 6 number grades divided by 6)
+averageGrade =
+
+#print the user's letter grade average based on their averageGrade. Remember, if the averageGrade is greater than or equal to 90, the letter grade is an A. If the averageGrade is greater than or equal to 80, the letter grade is a B. If the averageGrade is greater than or equal to 70, the letter grade is a C. If the averageGrade is greater than or equal to 60, the letter grade is a D. If the averageGrade is less than 60, the letter grade is an F. (HINT: I recommend using 5 condition statements in total, including 1 if statement, 3 elif statements, and an else statement)
+
+
+
+#BONUS: add the appropriate if and else statements that tell the user if they can play sports
