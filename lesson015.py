ddad2a9bd7839d2213e5bd631370d081a9af925c
Mark Peters <mapeters101@earthlink.net>
2025-01-04 23:27:04 -0500

    Work on lesson015 to 2:10.

---
+++ lesson015.py
@@ -0,0 +1,24 @@
+print("-----#1-----")
+if 1 == 1:
+    print(True)
+else:
+    print(False)
+
+print("-----#2-----")
+if 1 != 2 and 2 == 3:
+    print(True)
+else:
+    print(False)
+
+print("-----#3-----")
+if 1 == 2 or 2 == 2:
+    print(True)
+else:
+    print(False)
+
+print("-----#4-----")
+if (1 + 1) == 2 and (3 / 2) == 1:
+    print(True)
+else:
+    print(False)
+
