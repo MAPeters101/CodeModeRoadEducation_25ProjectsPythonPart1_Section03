72f0fcb83a1d5e877449104961cf886a67504603
Mark Peters <mapeters101@earthlink.net>
2025-01-05 01:10:30 -0500

    Partial code for assignment07.

--- assignment07.py
+++ assignment07.py
@@ -5,29 +5,48 @@ print("\nAvailable Pizza Sizes:")
 print("1. Small ($10) \n2. Medium ($15) \n3. Large ($20)")
 
 #ask the user what size they want by typing in a certain number (HINT: wrap the input around an int() so that you can use the input variable as a number)
+pizza_size = int(input("Please enter the size of pizza you would like: "))
 
 #use an if statement to check if the size that the user entered is greater than 3 or less than 1
-
+if pizza_size > 3 or pizza_size < 1:
+    print("Invalid entry, you will not get a pizza.")
 #inside of an else statement, you will increase the value of the price variable by 10 if the user entered 1, 15 if the user entered 2, and 20 if the user entered 3 for the size input
+else:
+    if pizza_size == 1:
+        price += 10
+    elif pizza_size == 2:
+        price += 15
+    if pizza_size == 3:
+        price += 20
 
-print("\nAvailable Pizza Toppings:")
-print(
-    "1. Pepperoni ($2) \n2. Mushrooms ($1.50) \n3. Onions ($1) \n4. Extra Cheese ($1.75)"
-)
+    print("\nAvailable Pizza Toppings:")
+    print(
+        "1. Pepperoni ($2) \n2. Mushrooms ($1.50) \n3. Onions ($1) \n4. Extra Cheese ($1.75)"
+    )
 
-#ask the user what topping (ONLY ONE) they want by typing in a certain number (HINT: wrap the input around an int() so that you can use the input variable as a number)
-
-#use an if statement to check if the topping that the user entered is greater than 4 or less than 1. This means that they selected a topping that was not in the menu.
-
-#inside of an else statement, you will increase the value of the price variable by 2 if the user entered 1, 1.5 if the user entered 2, 1 if the user entered 3, and 1.75 if the user entered 4 for the topping input
+    #ask the user what topping (ONLY ONE) they want by typing in a certain number (HINT: wrap the input around an int() so that you can use the input variable as a number)
+    pizza_toppings = int(input("Please select ONLY 1 topping: "))
+    #use an if statement to check if the topping that the user entered is greater than 4 or less than 1. This means that they selected a topping that was not in the menu.
+    if pizza_toppings > 4 or pizza_toppings < 1:
+        print("Invalid entry, you will not get any toppings.")
+    #inside of an else statement, you will increase the value of the price variable by 2 if the user entered 1, 1.5 if the user entered 2, 1 if the user entered 3, and 1.75 if the user entered 4 for the topping input
+    else:
+        if pizza_toppings == 1:
+            price += 2
+        if pizza_toppings == 2:
+            price += 1.5
+        if pizza_toppings == 3:
+            price += 1
+        if pizza_toppings == 4:
+            price += 1.75
 
 print("\nAvailable Drinks:")
 print("1. Soda ($2) \n2. Water ($1)")
 
 #ask the user what drink they want by typing in a certain number (HINT: wrap the input around an int() so that you can use the input variable as a number)
-
+drink = int(input("What drink would you like: "))
 #use an if statement to check if the drink that the user entered is greater than 2 or less than 1. This means that they selected a drink that was not in the menu.
-
+if drink > 2 or drink < 1:
 #inside of an else statement, you will increase the value of the price variable by 2 if the user entered 1, and 1 if the user entered 1 for the drink input
 
 #this is the final part of the code. Notice how it is commented out. This is because I would like you to start getting the hang of writing your own final print statements and using the format keyword. Just retype the code below, so that you get practice writing output code.
