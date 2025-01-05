8dee93bf119d44200e4c2e3da5d649628922ccc1
Mark Peters <mapeters101@earthlink.net>
2025-01-05 13:27:15 -0500

    Initial code for assignment08.

---
+++ assignment08.py
@@ -0,0 +1,40 @@
+import random
+
+# create a variable and name it something like "computerChoice". Store a random integer between 1 and 3 in the variable.
+
+
+# ask the user for their choice (if they choose rock, make them type 1, if they choose paper, make them type 2, and if they choose scissor, make them type 3)
+
+
+# check if the user entered a number between 1 and 3. if they didn't, then tell them that they have to restart and to "follow instructions"
+
+
+# else, first print out the computers choice (rock if the random integer    was 1, paper if the random integer was 2, and scissor if the random        integer was 3)
+
+
+# then check each possibility, and output who won in each of these possibilities
+
+# if the user entered 1 and computer entered 1 then it is a tie game
+
+
+# if the user entered 2 and computer entered 2 then it is a tie game
+
+
+# if the user entered 3 and computer entered 3 then it is a tie game
+
+
+# if the user entered 1 and computer entered 2 then the computer won
+
+
+# if the user entered 2 and computer entered 3 then the computer won
+
+
+# if the user entered 3 and computer entered 1 then the computer won
+
+
+# if the user entered 1 and computer entered 3 then the user won
+
+
+# if the user entered 2 and computer entered 1 then the user won
+
+# if the user entered 3 and computer entered 2 then the user won
