da03f9a2fa51ced4c622ff3cacf47495bf8686e1
Mark Peters <mapeters101@earthlink.net>
2025-01-04 22:42:54 -0500

    Complete work on lesson014.

--- lesson014.py
+++ lesson014.py
@@ -5,3 +5,18 @@ if name[0].upper() == 'S':
 else:
     print("Your name does not start with an S.")
 
+import random
+
+num = int(input("Enter a number between 1 an 100: "))
+mystery = random.randint(1, 100)
+
+if num > mystery:
+    print("Too high!")
+elif num < mystery:
+    print("Too low!")
+else:
+    print("You got it!")
+
+print(f"The mystery number is {mystery}")
+
+
